e11acba0f5a8153182764bf1643f290f1c6e44c3
EHSANJiSHAD <tanvirehsan60@gmail.com>
2022-05-17 01:38:45 +0600

    old fake data fetched from db to client side

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -9,7 +10,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//////////////////MONGODB CONNECT/////////////////
 
+const uri = `mongodb+srv://${process.env.DB_USER1}:${process.env.DB_PASSWORD1}@cluster0.gbtik.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const bookCollection = client.db('banglaBoi').collection('book');
+
+        app.get('/book',async(req,res)=>{
+            const query = {};
+            const cursor = bookCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+//////////////////MONGODB CONNECT/////////////////
 
 app.get('/',(req,res)=>{
     res.send('RUNNING BANGLA-BOI SERVER');
