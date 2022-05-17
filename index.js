9a06f7fccab95a66ba40f0e8f2ea51b2028fbe32
EHSANJiSHAD <tanvirehsan60@gmail.com>
2022-05-17 09:27:10 +0600

    single item received from db

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -20,12 +20,24 @@ async function run(){
         await client.connect();
         const bookCollection = client.db('banglaBoi').collection('book');
 
+
+        ////////////////GET ALL ITEMS///////////////////
         app.get('/book',async(req,res)=>{
             const query = {};
             const cursor = bookCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         })
+        ////////////////GET ALL ITEMS///////////////////
+
+        /////////////////GET SINGLE ITEM////////////
+        app.get('/book/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bookCollection.findOne(query);
+            res.send(result);
+        })
+        /////////////////GET SINGLE ITEM////////////
     }
     finally{
 
