be2efbb681dfd05515336397e532d2114eeccc06
EHSANJiSHAD <tanvirehsan60@gmail.com>
2022-05-18 03:06:04 +0600

    delete item from db added

--- index.js
+++ index.js
@@ -74,6 +74,15 @@ async function run(){
         })
 
         ////////////////UPDATE ITEM /////////////////
+
+
+         //DELETE AN ITEM
+         app.delete('/book/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await bookCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally{
 
